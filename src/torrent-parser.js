871d452314cf3f83a0eff3fdb8b62f25c0323ded
brandopakel <brando.pakel@gmail.com>
2022-02-01 16:35:07 -0800

    initial commit

---
+++ src/torrent-parser.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const bencode = require('bencode');
+const crypto = require('crypto');
+
+module.exports.BLOCK_LEN = Math.pow(2,14);
+
+module.exports.pieceLen = (torrent,pieceIndex) => {
+    const totalLength = BigInt.fromBuffer(this.size(torrent)).toNumber();
+    const pieceLength = torrent.info['piece length'];
+
+    const lastPieceLength = totalLength%pieceLength;
+    const lastPieceIndex = Math.floor(totalLength/pieceLength);
+
+    return lastPieceIndex === pieceIndex ? lastPieceLength : pieceLength;
+};
+
+module.exports.blocksPerPiece = (torrent, pieceIndex) => {
+    const pieceLength = this.pieceLen(torrent,pieceIndex);
+    return Math.ceil(pieceLength/this.BLOCK_LEN);
+};
+
+module.exports.blockLen = (torrent,pieceIndex,blockIndex) => {
+    const pieceLength = this.pieceLen(torrent,pieceIndex);
+
+    const lastPieceLength = pieceLength % this.BLOCK_LEN;
+    const lastPieceIndex = Math.floor(pieceLength/this.BLOCK_LEN);
+
+    return blockIndex === lastPieceIndex ? lastPieceLength : this.BLOCK_LEN;
+};
+
+module.exports.open = (filepath) => {
+    return bencode.decode(fs.readFileSync(filepath));
+};
+
+module.exports.size = (torrent) => {
+    const size = torrent.info.files ?
+        torrent.info.files.map(file => file.length).reduce((a,b) => a + b):
+        torrent.info.length;
+    
+    return BigInt.toBuffer(size, {size:8});
+};
+
+module.exports.infoHash = (torrent) => {
+    const info = bencode.encode(torrent.info);
+    return crypto.createHash('sha1').update(info).digest();
+};
